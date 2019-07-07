65fe03261aca742910fcb1fd07f443d37e3190e1
mark bashir <beejayphil@gmail.com>
2019-07-07 10:44:54 +0100

    increased the over all height of the app to 1200px

--- src/404/404.js
+++ src/404/404.js
@@ -10,7 +10,7 @@ export default function FourOFour() {
     <div className="error">
       <h1>Page Not Found!!! </h1>
       <div style={{width:"20%",margin:"auto" , textAlign: "center", padding: "5px"}} >
-						<input style={{borderRadius: "5px",fontSize:"1.4rem",}} onClick={handleClick} className="suscess outline-success " type="button" value="go back" />
+						<input style={{borderRadius: "5px",fontSize:"1.4rem",cursor:"pointer"}} onClick={handleClick} className="suscess outline-success " type="button" value="go back" />
 					</div>
       <img className="gif-block" src={errorImage} alt="error page" />
       
