56a3afdb2a9e889a320e79162e71e4908aed43e4
mark bashir <beejayphil@gmail.com>
2019-08-01 05:40:35 +0100

    added supertest

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const usersRouter = require('./routes/users');
+const graqhqlHTTP = 'express-graphql';
 require('../server/src/database/database.js'); 
 
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/graphql',graqp)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
