e649788e8f55d5dbd9f73281c3c4274cca45804a
mark bashir <beejayphil@gmail.com>
2019-08-08 23:34:07 +0100

    added documentation to function

--- server/src/controller/user.js
+++ server/src/controller/user.js
@@ -1,27 +1,49 @@
 const User = require('../model/user');
+const uuid = require('uuid/v4');
 
+/**
+ * @description controller to create user
+ * @author "mark bashir"
+ * @date 2019-08-08
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} password
+ * @param {string} phone
+ * @param {string} gender
+ * @param {string} email
+ * @returns user details or string
+ */
 async function createUser(firstName, lastName, password, phone, gender, email) {
-      const user = new User({ firstName, lastName, password, phone, gender, email });
-      const result = await user.save();
+  const id = uuid();
+  const user = new User({
+    id,
+    firstName,
+    lastName,
+    password,
+    phone,
+    gender,
+    email,
+  });
+  const result = await user.save();
   if (result) return result;
   return 'mail already used';
 }
-function getUserAccount(email, password) {
+function getUser(id) {
   return new Promise(async (resolve, reject) => {
     try {
-      const user = await User.findOne({ email, password }).select({
+      const user = await User.findOne({ id }).select({
         firstName: 1,
         lastName: 1,
         gender: 1,
         email: 1,
         phone: 1,
         imageUrl: 1,
-        services:1
-
+        services: 1,
       });
-      if (user===null) { throw new Error('account not found') }
+      if (user === null) {
+        throw new Error('account not found');
+      }
       resolve(user);
-
     } catch (error) {
       reject(error);
     }
@@ -30,35 +52,37 @@ function getUserAccount(email, password) {
 function updateUser(mail, password, details) {
   return new Promise(async (resolve, reject) => {
     try {
-
       const result = await User.findOneAndUpdate(
         {
-          email: mail, password
+          email: mail,
+          password,
         },
         {
-          $set:
-            details
-
+          $set: details,
         },
-        { new: true }
+        { new: true },
       );
       if (result === null) {
-        throw new Error("incorrect details");
+        throw new Error('incorrect details');
       }
 
       let { firstName, lastName, phone, gender, email } = result;
       resolve({ firstName, lastName, phone, gender, email });
-
     } catch (error) {
       reject(error.message);
     }
   });
 }
-function getSingleUser(userID){
-  return new Promise(async(resolve,reject)=>{
+function getSingleUser(userID) {
+  return new Promise(async (resolve, reject) => {
     try {
-      user=await User.aggregate([{$match:{'_id':mongoose.Types.ObjectId(userID)}},{$project:{'password':0,'__v':0,'email':0}},{$unwind:'$services'},{$match:{'services.published':true}}]);
-    resolve(user);
+      user = await User.aggregate([
+        { $match: { _id: mongoose.Types.ObjectId(userID) } },
+        { $project: { password: 0, __v: 0, email: 0 } },
+        { $unwind: '$services' },
+        { $match: { 'services.published': true } },
+      ]);
+      resolve(user);
     } catch (error) {
       reject(error.message);
     }
@@ -67,18 +91,18 @@ function getSingleUser(userID){
 function updatePassword(email, password, newPassword) {
   return new Promise(async (resolve, reject) => {
     try {
-      const result = await User.findOneAndUpdate({ email, password },
+      const result = await User.findOneAndUpdate(
+        { email, password },
         {
           $set: {
-            password: newPassword
-          }
-
-        });
+            password: newPassword,
+          },
+        },
+      );
       if (result === null) {
         throw new Error('incorrect details');
       }
       resolve('password updated');
-
     } catch (error) {
       reject(error.message);
     }
@@ -88,66 +112,101 @@ function getAllUsers() {
   return new Promise(async (resolve, reject) => {
     try {
       const users = await User.find().select();
-      if (users.length === 0) { throw new Error('no users') }
-      user=User.aggregate([{$sort:{'firstName':1,'lastName':1}},{$project:{'services':0,'password':0,'__v':0}}]);
+      if (users.length === 0) {
+        throw new Error('no users');
+      }
+      user = User.aggregate([
+        { $sort: { firstName: 1, lastName: 1 } },
+        { $project: { services: 0, password: 0, __v: 0 } },
+      ]);
       resolve(user);
-
     } catch (error) {
       reject(error);
     }
   });
 }
-function createService( email, role, serviceTitle, category, description, published = false) {
+function createService(
+  email,
+  role,
+  serviceTitle,
+  category,
+  description,
+  published = false,
+) {
   return new Promise(async (resolve, reject) => {
     try {
       let result = await User.findOne({ email });
-       if (result === null) throw new Error('user not found');
-      
-      let {services,firstName,lastName }=result;
+      if (result === null) throw new Error('user not found');
+
+      let { services, firstName, lastName } = result;
 
       if (services.length >= 5) {
         throw new Error('can not have more than 5 services');
-      } else { 
-        services.push({ userName: `${firstName} ${lastName}`, role, serviceTitle, category, description, published });
+      } else {
+        services.push({
+          userName: `${firstName} ${lastName}`,
+          role,
+          serviceTitle,
+          category,
+          description,
+          published,
+        });
         user = await result.save();
         resolve(user.services);
       }
-    
-
     } catch (error) {
       reject(error.message);
     }
   });
 }
-function getAllServices() { 
+function getAllServices() {
   return new Promise(async (resolve, reject) => {
     try {
-      result = await User.aggregate([{$unwind:'$services'},{$project:{'services':1,_id:0}},{$match:{'services.published':true}}]);
-     
+      result = await User.aggregate([
+        { $unwind: '$services' },
+        { $project: { services: 1, _id: 0 } },
+        { $match: { 'services.published': true } },
+      ]);
+
       resolve(result);
     } catch (error) {
       reject(error.message);
     }
-   });
+  });
 }
-function publishService(email,serviceID) {
-return new Promise(async(resolve,reject)=>{
-try {
-  // result = await User.findOne({email});
-  // if(result===null){
-  //   throw new Error('user not found');
-  // }
-  service= await User.aggregate([{$unwind:'$services'},{$project:{'services':1,_id:0}},{$match:{'services.published':false,'services._id':mongoose.Types.ObjectId(serviceID)}}]);
-  if(service.length===0) throw new Error('service already published');
-  resolve(service)
-} catch (error) {
-  reject(error.message);
-}
-});
+function publishService(email, serviceID) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // result = await User.findOne({email});
+      // if(result===null){
+      //   throw new Error('user not found');
+      // }
+      service = await User.aggregate([
+        { $unwind: '$services' },
+        { $project: { services: 1, _id: 0 } },
+        {
+          $match: {
+            'services.published': false,
+            'services._id': mongoose.Types.ObjectId(serviceID),
+          },
+        },
+      ]);
+      if (service.length === 0) throw new Error('service already published');
+      resolve(service);
+    } catch (error) {
+      reject(error.message);
+    }
+  });
 }
 
 let user = getSingleUser('5d441be4ab9fe2b95c5f186b');
 //  let user= createService("chiderahopewell@gmail.com","technical writing","Software service","all industries","offer software development services",true)
 // module.exports={createUser,getUser,getAllUsers,updateUser,updatePassword,createService};
 // user=createUser('chidera','stephen','zinachi','08130302988','female','chiderahopewell@gmail.com');
-  user.then((data) => { console.log(data) }).catch((data) => { console.log(data) });
+user
+  .then(data => {
+    console.log(data);
+  })
+  .catch(data => {
+    console.log(data);
+  });
